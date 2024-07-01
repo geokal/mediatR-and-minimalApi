4a1e260b15ff8f58ed6129d30b57f564fb8cb458
geokal <kalpgiorgos@gmail.com>
2024-07-01 10:53:36 +0300

    added Otel

--- src/Blogger/Blogger.API/Program.cs
+++ src/Blogger/Blogger.API/Program.cs
@@ -4,6 +4,10 @@ using Blogger.API.Requests.Commands;
 using Blogger.API.Requests.Queries;
 using Blogger.API.Services;
 using MediatR;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 
 
@@ -27,6 +31,32 @@ try
     builder.Services.AddMemoryCache();
     builder.Services.AddAutoMapper(typeof(Program));
     builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+    // Build a resource configuration action to set service information.
+    // Action<ResourceBuilder> configureResource = r => r.AddService(
+    // serviceName: "otel-test",
+    // serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+    // serviceInstanceId: Environment.MachineName);
+    
+    builder.Services.AddOpenTelemetry()
+    //.ConfigureResource(configureResource)
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();
+    })
+    .WithMetrics(metrics => metrics
+          .AddRuntimeInstrumentation()
+          // Metrics provides by ASP.NET Core in .NET 8
+        .AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel", "System.Net.Http")
+    );
+
+    var useOtlpExporter = !string.IsNullOrWhiteSpace(builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
+    if (useOtlpExporter)
+    {
+        builder.Services.AddOpenTelemetry().UseOtlpExporter();
+    }
 
     var app = builder.Build();
 
@@ -53,7 +83,7 @@ try
     }
     app.UseSerilogRequestLogging();
     app.UseHttpsRedirection();
-
+    //app.UseOpenTelemetryPrometheusScrapingEndpoint();
     // app.MapControllers();
 
     app.Run();
